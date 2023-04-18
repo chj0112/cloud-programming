1f7c2671fa6b31ba32aa742c49349b1128d14043
chj0112 <chohj000112@gmail.com>
2023-04-18 15:50:23 +0900

    마크다운, 폼 추가

--- django_project/blog/urls.py
+++ django_project/blog/urls.py
@@ -10,5 +10,5 @@ urlpatterns = [
     path('update_post/<int:pk>/', views.PostUpdate.as_view()),
     path('<int:pk>/', views.PostDetail.as_view()),
     path('category/<str:slug>/', views.categories_page),
-    path('tag/<str:slug>/', views.tag_page)
+    path('tag/<str:slug>/', views.tag_page),
 ]
